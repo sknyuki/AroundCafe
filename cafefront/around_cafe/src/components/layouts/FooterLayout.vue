f457b7cd0b0428e041948cb1b9b8f620d5e89d6e
Mong9 <bean326@naver.com>
2022-05-26 20:56:37 +0900

    ✅ scss set test

---
+++ cafefront/around_cafe/src/components/layouts/FooterLayout.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div>푸터다</div>
+  </div>
+</template>
+<script>
+export default {
+  name : "FooterLayout"
+  
+}
+</script>
+<style lang="">
+  
+</style>
---
+++ cafefront/around_cafe/src/components/layouts/HeaderLayout.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div>헤더다</div>
+  </div>
+</template>
+<script>
+export default {
+  name : "HeaderLayout"
+  
+}
+</script>
+<style lang="">
+  
+</style>
