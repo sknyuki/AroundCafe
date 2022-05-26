25e89d0ab35bbe1dd6fafad4ef16acf2741339e4
jiyeon12345 <wldys5353@naver.com>
2022-05-26 20:41:57 +0900

    백앤드_초기세팅

--- cafeback/AroundCafe/src/main/java/com/example/demo/cafeMypage/controller/CafeController.java
+++ cafeback/AroundCafe/src/main/java/com/example/demo/cafeMypage/controller/CafeController.java
@@ -1,14 +1,13 @@
-package com.example.demo.controller;
+package com.example.demo.cafeMypage.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-@RequestMapping("/cafe_mypage")
+@RequestMapping("/cafe")
 @CrossOrigin(origins = "http://localhost:8080", allowedHeaders = "*")
 public class CafeController {
 
