8d53950baddbf1559a77ac01ba08791732654e04
Mong9 <bean326@naver.com>
2022-05-29 14:04:30 +0900

    ♻️Refactoring MainPage[FMP]

--- cafefront/around_cafe/src/views/MainPage.vue
+++ cafefront/around_cafe/src/views/MainPage.vue
@@ -1,18 +1,9 @@
-<template >
-  <section>
-    <v-container>
-      <v-row>
-        <v-col>
-          <gallery-card></gallery-card>
-        </v-col>
-      </v-row>
-    </v-container>
-  </section>
+<template>
+  <GalleryCard />
 </template>
 <script>
-import GalleryCard from '@/components/gallery/GalleryCard.vue'
+import GalleryCard from "@/components/gallery/GalleryCard.vue"
 export default {
   components: { GalleryCard },
-  
 }
 </script>
