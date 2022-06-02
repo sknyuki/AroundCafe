d7f92ac35e76c99fa61a917e23c2205b0e2af9b3
jiyeon12345 <wldys5353@naver.com>
2022-06-02 21:46:54 +0900

    카페 마이페이지_메뉴 삭제 완료[FCMPS]

--- cafefront/around_cafe/src/components/MyPageComponents/CafeMypageComponents/CafeMenuListForm.vue
+++ cafefront/around_cafe/src/components/MyPageComponents/CafeMypageComponents/CafeMenuListForm.vue
@@ -28,6 +28,45 @@
                     </template>
             </v-data-table>
             </v-card>
+            
+        </div>
+        <br>
+        <div>
+            <h4>등록한 메뉴 확인</h4>
+            <v-card>
+            <v-card-title>
+            <v-text-field
+                v-model="search"
+                append-icon="mdi-magnify"
+                label="메뉴 찾기"
+                single-line
+                hide-details
+            ></v-text-field>
+            </v-card-title>
+            <v-data-table :headers="headerTitle1" :items="menuLists" :search="search"  class="elevation-0" >
+                    
+                    <template v-slot:[`item.linkInfo`]="{ item }" >
+                        <v-img v-if="item.menu_img != null" v-bind:src="require(`@/assets/cafe/cafeMenu/${item.menu_img}`)" height="50px"/>
+                        <v-img v-if="item.menu_img != null" v-bind:src="require(`@/assets/cafe/cafeMenu/imgNull.png`)" height="50px"/>
+                    </template>   
+
+                    <template v-slot:[`item.actions`] ="{ item }" >
+                    <v-icon
+                        small
+                        class="mr-2"
+                        @click="editItem(item)"
+                    >
+                        mdi-pencil
+                    </v-icon>
+                    <v-icon
+                        small
+                        @click="deleteItem(item)"
+                    >
+                        mdi-delete
+                    </v-icon>
+                    </template>
+            </v-data-table>
+            </v-card>
 
             <form @submit.prevent="modifySubmit">
                 <v-dialog v-model="dialog" >
@@ -58,84 +97,6 @@
                 </v-dialog>
             </form>
                 
-            <!-- <v-dialog v-model="deleteDialog">
-                <v-card>
-                    <v-card-title class="headline">정말 삭제하시겠습니까?</v-card-title>
-                    <v-card-text>삭제하게 되면 내용은 다시 볼 수 없게 됩니다.</v-card-text>
-                    <v-card-actions>
-                    <v-spacer></v-spacer>
-                    <v-btn color="white" @click="deleteDialog = false">돌아가기</v-btn>
-                    <v-btn color="white" type="button" @click="deleteReview">삭제합니다</v-btn>
-                    </v-card-actions>
-                </v-card>   
-            </v-dialog> -->
-        </div>
-        <br>
-        <div>
-            <h4>등록한 메뉴 확인</h4>
-            <v-card>
-            <v-card-title>
-            <v-text-field
-                v-model="search"
-                append-icon="mdi-magnify"
-                label="메뉴 찾기"
-                single-line
-                hide-details
-            ></v-text-field>
-            </v-card-title>
-            <v-data-table :headers="headerTitle1" :items="menuLists" :search="search"  class="elevation-0" >
-                    
-                    <template v-slot:[`item.linkInfo`]="{ item }" >
-                        <img v-if="item.linkInfo != null" v-bind:src="require(`@/assets/cafe/cafeMenu/${item.menu_img}`)" height="230px"/>
-                    </template>   
-
-                    <template v-slot:[`item.actions`] ="{ item }" >
-                    <v-icon
-                        small
-                        class="mr-2"
-                        @click="editItem(item)"
-                    >
-                        mdi-pencil
-                    </v-icon>
-                    <v-icon
-                        small
-                        @click="deleteItem(item)"
-                    >
-                        mdi-delete
-                    </v-icon>
-                    </template>
-            </v-data-table>
-            </v-card>
-
-            <!-- <form @submit.prevent="modifySubmit">
-                <v-dialog v-model="dialog" >
-                    <v-card >
-                        <v-card-title >수정하기</v-card-title>
-                        <v-card-text>
-                            <v-container>
-                                <v-row>
-                                    <v-col>
-                                        <p>파일 다시 올리기</p>
-                                        <input type="file" id="files2" ref="files2"
-                                            multiple v-on:change="handleFileUpload2()"/>
-                                    </v-col>
-                                    <v-col>
-                                        <v-select class="selectCake" v-model="design" :items="selectCake" label="디자인 선택"/>
-                                        <v-select class="selectSize" v-model="size" :items="selectSize" label="사이즈 선택"/>
-                                        <v-text-field label="가격 작성" v-model="price"></v-text-field>
-                                    </v-col>
-                                </v-row>
-                            </v-container>
-                        </v-card-text>
-                        <v-card-actions>
-                        <v-spacer></v-spacer>
-                        <v-btn color="white" @click="dialog = false">돌아가기</v-btn>
-                        <v-btn color="white" @click="modifySubmit">수정하기</v-btn>
-                        </v-card-actions>
-                    </v-card>   
-                </v-dialog>
-            </form>
-                
             <v-dialog v-model="deleteDialog">
                 <v-card>
                     <v-card-title class="headline">정말 삭제하시겠습니까?</v-card-title>
@@ -146,7 +107,7 @@
                     <v-btn color="white" type="button" @click="deleteReview">삭제합니다</v-btn>
                     </v-card-actions>
                 </v-card>   
-            </v-dialog> -->
+            </v-dialog> 
         </div>
     </div>
 </template>
@@ -214,7 +175,7 @@ import axios from 'axios'
             },
             deleteItem(item){
                 this.deleteDialog = true
-                this.deleteNo = item.cakeNo
+                this.deleteNo = item.menu_no
             },
             handleFileUpload2() {
                 this.files2 = this.$refs.files2.files
@@ -225,9 +186,9 @@ import axios from 'axios'
             },
             deleteReview() {
                 
-                this.cakeNo = this.deleteNo
+                this.menuNo = this.deleteNo
 
-                axios.post(`http://localhost:7777/upload/delete/${this.cakeNo}` )
+                axios.delete(`http://localhost:7777/menu/delete/${this.menuNo}` )
                         .then(() => {
                             alert('삭제가 완료되었습니다!')
                             this.$router.go()
