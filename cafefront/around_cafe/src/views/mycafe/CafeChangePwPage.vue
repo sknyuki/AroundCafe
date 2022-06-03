c649406d4b5e7b436ab69010a755084840cabb41
Mong9 <bean326@naver.com>
2022-06-03 19:07:21 +0900

    ✅ Add CafeChangePwPage and ChangePwForm.vue

---
+++ cafefront/around_cafe/src/views/mycafe/CafeChangePwPage.vue
@@ -0,0 +1,12 @@
+<template>
+  <CafeChangePw />
+</template>
+<script>
+import CafeChangePw from "@/components/cafe/CafeChangePw.vue"
+export default {
+  name: "CafeChangePwPage",
+  components: {
+    CafeChangePw,
+  },
+}
+</script>
