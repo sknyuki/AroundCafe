a63ddb7f6cf7474484d45dea72bb7d33078e5c08
jiyeon12345 <wldys5353@naver.com>
2022-06-04 16:40:14 +0900

    카페 마이페이지_카페 내역 수정(이미지 등록 실패)[FCMPS]

--- cafefront/around_cafe/src/store/mutations.js
+++ cafefront/around_cafe/src/store/mutations.js
@@ -1,7 +1,6 @@
 import {
-    //CafeMyPage 작업시 열어주세요
-  /*  FETCH_CAFE_BOARD_LIST,
-    FETCH_CAFE_BOARD*/
+    FETCH_CAFE_BOARD_LIST,
+    FETCH_CAFE_BOARD,
     FETCH_MENU_LISTS,
 
 
@@ -9,15 +8,13 @@ import {
 
 export default {
 
-    //CafeMyPage
-
-    /*[FETCH_CAFE_BOARD_LIST] (state, cafeBoards) {
+    [FETCH_CAFE_BOARD_LIST] (state, cafeBoards) {
         state.cafeBoards = cafeBoards
    },
 
     [FETCH_CAFE_BOARD ] (state, cafeBoard) {
         state.cafeBoard = cafeBoard
-   },*/
+   },
 
     [FETCH_MENU_LISTS] (state, menuLists) {
     state.menuLists = menuLists
