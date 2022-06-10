28668679bbc4fa82a631cad1d0762bf30abdd167
flowraker <flowraker@gmail.com>
2022-06-10 09:59:44 +0900

    OAuth2.0 작업[BMMS]

---
+++ cafeback/AroundCafe/src/main/java/com/example/demo/security/jwt/dto/OAuth2Request.java
@@ -0,0 +1,15 @@
+package com.example.demo.security.jwt.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class OAuth2Request {
+    private String socialType;
+    private String socialNo;
+    private String email; // memId
+    private String birthday; // memBirth
+    private String phoneNum;
+    private String imageUrl;
+}
