2107620e11a9cfce03a0ac70c651f2104769390e
legojinK <ckgksck@gmail.com>
2022-06-15 23:39:09 +0900

    후기 수정 다이얼로그 X [FRP]

---
+++ cafefront/around_cafe/src/views/Cafe/CafeReviewModifyPage.vue
@@ -0,0 +1,92 @@
+<template>
+    <v-container class="writeForm">
+        <v-row>
+        
+            <cafe-review-list v-if="review" :review="review" />
+
+            <v-dialog max-width="750" v-model="reviewDialog">
+            </v-dialog> 
+            <CafeReviewModify @submit="onSubmit"/>
+          
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+import axios from 'axios'
+import { mapActions,mapState } from 'vuex'
+import CafeReviewModify from "@/components/Cafe/CafeReviewModify"
+import CafeReviewList from "@/components/Cafe/CafeReviewList"
+
+export default {
+    name: 'CafeReviewModifyPage',
+    components: { 
+        CafeReviewModify,
+        CafeReviewList
+    
+    },
+     props: {
+        reviewNo: {
+            type: String,
+            required: true
+        }
+    },
+    
+    data() {
+    return {
+      reviewDialog: false,
+    }
+  },
+  computed: {
+        ...mapState(['review'])
+    },
+    
+    methods: {
+         ...mapActions(['fetchReview']),
+        onSubmit (payload) {
+            const { star_score, review_content, cafeNum,file} = payload
+            let formData = new FormData()
+            if (file != null )
+            {formData.append('file', file)}
+            formData.append('star_score',star_score)
+            formData.append('review_content', review_content)
+            formData.append('cafeNum', cafeNum)
+            
+           
+            axios.post(`http://localhost:7777/cafe/review/${this.reviewNo}`,formData, { headers: {
+                    'Content-Type': 'multipart/form-data'
+                }})
+                    .then(() => {
+                        alert("Successfully submitted")
+                        this.$router.push({
+                            name: 'CafeReviewListPage'
+                        })
+                    })
+                    .catch(() => {
+                        alert('문제 발생!')
+                    })
+        },
+    onReviewDialog() {
+      this.reviewDialog = true
+    },
+    closeDialog() {
+      this.reviewDialog = false
+    },
+    created() {
+        this.fetchReview(this.reviewNo)
+        .catch(() => {
+            alert('게시물 DB조회 실패!')
+        })
+    }
+  },
+}
+
+</script>
+
+<style lang="scss" scoped>
+::v-deep .v-dialog {
+  @include no-scrollbar;
+  overflow-y: auto;
+  overflow-x: hidden;
+}
+</style>e>
