b3b6eb8b59ffd3b05989ea89db2e2f5e572e3787
Mong9 <bean326@naver.com>
2022-06-16 23:36:01 +0900

    ✅ Add Skeleton UI

--- cafefront/around_cafe/src/components/SkeletonBox.vue
+++ cafefront/around_cafe/src/components/SkeletonBox.vue
@@ -1,38 +1,10 @@
 <template>
-  <span :style="{ height, width: computedWidth }" class="SkeletonBox" />
+  <span class="SkeletonBox" />
 </template>
 
 <script>
 export default {
   name: "SkeletonBox",
-  props: {
-    maxWidth: {
-      default: 100,
-      type: Number,
-    },
-    minWidth: {
-      default: 80,
-      type: Number,
-    },
-    height: {
-      default: "1em",
-      type: String,
-    },
-    width: {
-      default: null,
-      type: String,
-    },
-    computed: {
-      computedWidth() {
-        return (
-          this.width ||
-          `${Math.floor(
-            0.9 * (this.maxWidth - this.minWidth) + this.minWidth
-          )}%`
-        )
-      },
-    },
-  },
 }
 </script>
 <style lang="scss" scoped>
