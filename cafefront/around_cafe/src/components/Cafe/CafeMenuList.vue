0946fcf68721f33f1df0432d137085bf28563bbc
jiyeon12345 <wldys5353@naver.com>
2022-06-23 22:08:52 +0900

    카페마이페이지 메뉴 수정,삭제 기능 추가[FCMPS]

--- cafefront/around_cafe/src/components/Cafe/CafeMenuList.vue
+++ cafefront/around_cafe/src/components/Cafe/CafeMenuList.vue
@@ -3,44 +3,38 @@
     <ul class="menu-list">
       <div v-if="this.fileterArray.length == 0">
         <li v-for="item in listData" :key="item.menu_no" class="menu-item">
-          <a href="">
-            <div class="menu-card">
-              <div class="menu-card-image">
-                <v-img
-                  v-if="item.menu_img == null"
-                  v-bind:src="require(`@/assets/cafe/cafeMenu/imgNull.png`)"
-                />
-                <v-img
-                  v-if="item.menu_img != null"
-                  v-bind:src="
-                    require(`@/assets/cafe/cafeMenu/${item.menu_img}`)
-                  "
-                />
-                <div class="test">
-                  <span>Signature</span>
-                </div>
-              </div>
-
-              <div class="menu-card-info">
-                <div class="menu-card-title">{{ item.menu_name }}</div>
-                <div class="menu-card-detail">
-                  <p>
-                    {{ item.menu_content }}
-                  </p>
-                </div>
-                <div class="menu-card-price">
-                  {{ item.menu_price | pricePoint }}원
-                </div>
+          <div class="menu-card">
+            <div class="menu-card-image">
+              <img
+                v-if="item.menu_img == null"
+                v-bind:src="require(`@/assets/cafe/cafeMenu/imgNull.png`)"
+              />
+              <img
+                v-if="item.menu_img != null"
+                v-bind:src="require(`@/assets/cafe/cafeMenu/${item.menu_img}`)"
+              />
+              <div class="test">
+                <span>Signature</span>
               </div>
             </div>
-          </a>
+
+            <div class="menu-card-info">
+              <div class="menu-card-title">{{ item.menu_name }}</div>
+              <div class="menu-card-detail">
+                <p>
+                  {{ item.menu_content }}
+                </p>
+              </div>
+              <div class="menu-card-price">
+                {{ item.menu_price | pricePoint }}원
+              </div>
+            </div>
+          </div>
 
           <div class="menu-item-btn">
-            <button class="modify-button" aria-label="해당 메뉴 수정하기">
-              <i class="icBell"></i>
-            </button>
+            <CafeModifyDialog :item="item" @submit="onModifyMenu" />
             <button
-              @click="onDeleteMenu"
+              @click="onDeleteMenu(item)"
               class="delete-button"
               aria-label="해당 메뉴 삭제하기"
               type="button"
@@ -49,17 +43,21 @@
             </button>
           </div>
         </li>
+        <PaginationForm
+          :pageSetting="pageDataSetting(total, limit, block, this.page)"
+          @paging="pagingMethod"
+        />
       </div>
       <div v-else>
         <li v-for="item in fileterArray" :key="item.menu_no" class="menu-item">
           <a href="">
             <div class="menu-card">
               <div class="menu-card-image">
-                <v-img
+                <img
                   v-if="item.menu_img == null"
                   v-bind:src="require(`@/assets/cafe/cafeMenu/imgNull.png`)"
                 />
-                <v-img
+                <img
                   v-if="item.menu_img != null"
                   v-bind:src="
                     require(`@/assets/cafe/cafeMenu/${item.menu_img}`)
@@ -85,11 +83,12 @@
           </a>
 
           <div class="menu-item-btn">
-            <button class="modify-button" aria-label="해당 메뉴 수정하기">
+            <CafeModifyDialog :item="item" @submit="onModifyMenu" />
+            <!-- <button class="modify-button" aria-label="해당 메뉴 수정하기">
               <i class="icBell"></i>
-            </button>
+            </button> -->
             <button
-              @click="onDeleteMenu"
+              @click="onDeleteMenu(item)"
               class="delete-button"
               aria-label="해당 메뉴 삭제하기"
               type="button"
@@ -100,33 +99,29 @@
         </li>
       </div>
     </ul>
-    <PaginationForm
-      :pageSetting="pageDataSetting(total, limit, block, this.page)"
-      @paging="pagingMethod"
-    />
   </div>
 </template>
 <script>
 import axios from "axios"
 import PaginationForm from "@/components/PaginationForm.vue"
+import { mapState, mapActions } from "vuex"
+import CafeModifyDialog from "./CafeModifyDialog.vue"
 
 export default {
   name: "CafeMenuList",
   components: {
     PaginationForm,
+    CafeModifyDialog,
   },
   props: {
     fileterArray: {
       type: Array,
       required: true,
     },
-    menuLists: {
-      type: Array,
-      required: true,
-    },
   },
   data() {
     return {
+      dialog: false,
       signatureNo: "",
       modi_name: "",
       listData: [],
@@ -135,21 +130,29 @@ export default {
       block: 5,
       pageNo: "",
       total: "",
+      modifyNo: "",
+      modify_name: "",
+      modify_price: "",
+      modify_content: "",
     }
   },
+  computed: {
+    ...mapState(["menuLists"]),
+  },
   mounted() {
+    this.fetchMenuLists(2)
     this.pagingMethod(this.page)
   },
 
   methods: {
+    ...mapActions(["fetchMenuLists"]),
     onSignatureMenu(item) {
       this.signatureNo = item.menu_no
     },
-    onModifyMenu() {},
-    onDeleteMenu() {
-      this.menuNo = this.deleteNo
+    onDeleteMenu(item) {
+      let menuNo = item.menu_no
       axios
-        .delete(`http://localhost:7777/menu/delete/${this.menuNo}`)
+        .delete(`http://localhost:7777/menu/delete/${menuNo}`)
         .then(() => {
           alert("삭제가 완료되었습니다!")
           this.$router.go()
@@ -186,6 +189,46 @@ export default {
         list.push(index)
       }
       return { first, end, list, currentPage }
+    },
+    editItem(item) {
+      this.dialog = true
+      this.modifyNo = item.menu_no
+      this.modify_name = item.menu_name
+      this.modify_price = item.menu_price
+      this.modify_content = item.menu_content
+    },
+    onModifyMenu(payload) {
+      const { modifyNo, menu_name, menu_price, menu_content, file } = payload
+
+      let formData = new FormData()
+
+      let fileInfo = {
+        menu_no: modifyNo,
+        menu_name: menu_name,
+        menu_price: menu_price,
+        menu_content: menu_content,
+      }
+
+      formData.append(
+        "info",
+        new Blob([JSON.stringify(fileInfo)], { type: "application/json" })
+      )
+
+      if (file != null) {
+        formData.append("fileList", file)
+      }
+
+      console.log(fileInfo)
+      let cafeNo = 1
+      axios
+        .put(`http://localhost:7777/menu/modify/${cafeNo}`, formData)
+        .then(() => {
+          alert("수정되었습니다!")
+          this.$router.go()
+        })
+        .catch(() => {
+          alert("수정 실패!")
+        })
     },
   },
 }
