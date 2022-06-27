edc676b5a039c65dc1fb2410fa5c3544cb7cc487
Mong9 <bean326@naver.com>
2022-06-28 00:29:10 +0900

    cafe grid

--- cafefront/around_cafe/src/components/Cafe/CafeCard.vue
+++ cafefront/around_cafe/src/components/Cafe/CafeCard.vue
@@ -1,107 +1,113 @@
 <template>
-  <div class="cafe-gallery-content">
-    <ul class="cafe-list">
-      <li
-        v-for="(cafeItem, index) in cafeBoards"
-        :key="index"
-        class="cafe-item"
-        @mouseover="swiperBtn = index"
-        @mouseleave="swiperBtn = null"
-      >
-        <!-- 스켈레톤 UI -->
-        <template v-if="isLoading">
-          <div>
-            <SkeletonBox class="skeleton-box" />
-          </div>
-          <div>
-            <SkeletonBox class="skeleton-title" />
-          </div>
-          <div>
-            <SkeletonBox class="skeleton-location" />
-          </div>
-          <div>
-            <SkeletonBox class="skeleton-time" />
-          </div>
-          <div>
-            <SkeletonBox class="skeleton-star" />
-          </div>
-        </template>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-12 col-md-12 col-lg-12">
+        <div class="cafe-gallery-content">
+          <ul class="cafe-list">
+            <li
+              v-for="(cafeItem, index) in cafeBoards"
+              :key="index"
+              class="cafe-item"
+              @mouseover="swiperBtn = index"
+              @mouseleave="swiperBtn = null"
+            >
+              <!-- 스켈레톤 UI -->
+              <template v-if="isLoading">
+                <div>
+                  <SkeletonBox class="skeleton-box" />
+                </div>
+                <div>
+                  <SkeletonBox class="skeleton-title" />
+                </div>
+                <div>
+                  <SkeletonBox class="skeleton-location" />
+                </div>
+                <div>
+                  <SkeletonBox class="skeleton-time" />
+                </div>
+                <div>
+                  <SkeletonBox class="skeleton-star" />
+                </div>
+              </template>
 
-        <template v-else>
-          <a href="" @focus.tab="swiperBtn = index">
-            <!-- 좋아요 토글 -->
-            <button class="cafe-item-btn" type="button">
-              <i class="icHeart"></i>
-            </button>
+              <template v-else>
+                <a href="" @focus.tab="swiperBtn = index">
+                  <!-- 좋아요 토글 -->
+                  <button class="cafe-item-btn" type="button">
+                    <i class="icHeart"></i>
+                  </button>
 
-            <!-- 스와이퍼 -->
-            <swiper class="swiper" :options="swiperOption">
-              <swiper-slide
-                v-for="img in cafeItem.cafeImgs"
-                :key="img.cafeImgNo"
-                class="cafe-gallery-image"
-              >
-                <picture>
-                  <img
-                    loading="lazy"
-                    v-bind:src="
-                      require(`@/assets/cafe/cafeMypage/${img.cafe_img}`)
-                    "
-                    alt="카페이미지"
-                  />
-                </picture>
-              </swiper-slide>
-              <v-btn
-                fab
-                x-small
-                class="swiper-button-prev is-prev"
-                slot="button-prev"
-                aria-label="이전 이미지"
-                type="button"
-                v-show="swiperBtn === index"
-              >
-                <i class="icChevron" aria-hidden></i>
-              </v-btn>
-              <v-btn
-                fab
-                x-small
-                class="swiper-button-next is-next"
-                slot="button-next"
-                aria-label="다음 이미지"
-                type="button"
-                v-show="swiperBtn === index"
-              >
-                <i class="icChevron" aria-hidden></i>
-              </v-btn>
-              <div class="swiper-pagination" slot="pagination"></div>
-            </swiper>
+                  <!-- 스와이퍼 -->
+                  <swiper class="swiper" :options="swiperOption">
+                    <swiper-slide
+                      v-for="img in cafeItem.cafeImgs"
+                      :key="img.cafeImgNo"
+                      class="cafe-gallery-image"
+                    >
+                      <picture>
+                        <img
+                          loading="lazy"
+                          v-bind:src="
+                            require(`@/assets/cafe/cafeMypage/${img.cafe_img}`)
+                          "
+                          alt="카페이미지"
+                        />
+                      </picture>
+                    </swiper-slide>
+                    <v-btn
+                      fab
+                      x-small
+                      class="swiper-button-prev is-prev"
+                      slot="button-prev"
+                      aria-label="이전 이미지"
+                      type="button"
+                      v-show="swiperBtn === index"
+                    >
+                      <i class="icChevron" aria-hidden></i>
+                    </v-btn>
+                    <v-btn
+                      fab
+                      x-small
+                      class="swiper-button-next is-next"
+                      slot="button-next"
+                      aria-label="다음 이미지"
+                      type="button"
+                      v-show="swiperBtn === index"
+                    >
+                      <i class="icChevron" aria-hidden></i>
+                    </v-btn>
+                    <div class="swiper-pagination" slot="pagination"></div>
+                  </swiper>
 
-            <h1 class="cafe-list-title">{{ cafeItem.cafe_name }}</h1>
+                  <h1 class="cafe-list-title">{{ cafeItem.cafe_name }}</h1>
 
-            <div class="cafe-list-location">
-              <span
-                >{{ cafeItem.cafe_adr1 }} {{ cafeItem.cafe_adr2 }}
-                {{ cafeItem.cafe_adr3 }}</span
-              >
-            </div>
+                  <div class="cafe-list-location">
+                    <span
+                      >{{ cafeItem.cafe_adr1 }} {{ cafeItem.cafe_adr2 }}
+                      {{ cafeItem.cafe_adr3 }}</span
+                    >
+                  </div>
 
-            <div class="cafe-list-time">
-              <span>{{ cafeItem.cafe_time }}</span>
-            </div>
+                  <div class="cafe-list-time">
+                    <span>{{ cafeItem.cafe_time }}</span>
+                  </div>
 
-            <dl class="cafe-list-rating">
-              <div class="rating">
-                <dt>
-                  <i class="icStar"></i>
-                  <span class="visually-hidden">평점</span>
-                </dt>
-                <dd>{{ cafeItem.star }}</dd>
-              </div>
-            </dl>
-          </a>
-        </template>
-      </li>
-    </ul>
+                  <dl class="cafe-list-rating">
+                    <div class="rating">
+                      <dt>
+                        <i class="icStar"></i>
+                        <span class="visually-hidden">평점</span>
+                      </dt>
+                      <dd>{{ cafeItem.star }}</dd>
+                    </div>
+                  </dl>
+                </a>
+              </template>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
