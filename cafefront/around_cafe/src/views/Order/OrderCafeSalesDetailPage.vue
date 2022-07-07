769f41b5ec6800c284226f13e46811d10c48bc60
jiyeon12345 <wldys5353@naver.com>
2022-07-07 20:46:10 +0900

    카페마이페이지_매출관련 기능 구현(추가)[FUMPS]

---
+++ cafefront/around_cafe/src/views/Order/OrderCafeSalesDetailPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <OrderCafeSalesDetailList :findDate="findDate" />
+</template>
+
+<script>
+import OrderCafeSalesDetailList from "@/components/Order/OrderCafeSalesDetailList.vue"
+export default {
+  components: { OrderCafeSalesDetailList },
+  name: "OrderCafeSalesDetailPage",
+  props: {
+    findDate: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
